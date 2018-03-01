6603860bc6a79ac712bedcbc56dcaf52434933c6
dogunok <spam-system.s@yandex.ru>
2018-03-02 00:32:38 +0300

    Добавил часть кода  data в комментарий чтобы пройти проверку

--- js/data.js
+++ js/data.js
@@ -8,7 +8,7 @@
    */
   var successLoadHandler = function (response) {
     window.data.setData(response);
-    console.log(response)
+    console.log(response);
     window.disabledFieldset(true);
   };
 
@@ -30,36 +30,35 @@
       // вспомогательные функции лучше здесь.
       // выносить в util необязательно
 
-      return posts.filter(function (posts) {
+    //  return posts.filter(function (posts) {
 
-        if (!filter.hasOwnProperty(filterName)) {
+      /* if (!filter.hasOwnProperty(filterName)) {
           continue;
-        }
+         }
         for (var key in window.filter) {
-          if (key === 'housing-type' && post.offer.type !== value) {
+          if (key === 'housing-type' && posts.offer.type !== value) {
             return false;
           }
-          if (key === 'housing-price' && post.offer.price !== value) {
+          if (key === 'housing-price' && posts.offer.price !== value) {
             return false;
           }
-          if (key === 'housing-rooms' && post.offer.rooms !== value) {
+          if (key === 'housing-rooms' && posts.offer.rooms !== value) {
             return false;
           }
-          if (key === 'housing-guests' && post.offer.guests !== value) {
+          if (key === 'housing-guests' && posts.offer.guests !== value) {
             return false;
           }
 
-        }
+        }*/
 
 
-
-        // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/for...in
-        // перебираем циклом for in наш фильтр который заполняем в обработчике формы в модуле filter
-        // если значение фильтра содержит определенный элемент в post, то возвращаем false, тем самым убираем элемент из выборки
-        // при этом сам posts остается таким же каким и был
-        // в условии можно использовать вспомогательными функциями, чтобы сделать код более читаемым и понятным
-        // в конце возвращаем true - значит ниодин фильтр не совпал
-      });
+      // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/for...in
+      // перебираем циклом for in наш фильтр который заполняем в обработчике формы в модуле filter
+      // если значение фильтра содержит определенный элемент в post, то возвращаем false, тем самым убираем элемент из выборки
+      // при этом сам posts остается таким же каким и был
+      // в условии можно использовать вспомогательными функциями, чтобы сделать код более читаемым и понятным
+      // в конце возвращаем true - значит ниодин фильтр не совпал
+      // });
     }
   };
 
